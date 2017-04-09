6af3896bc8aa0e5862df6ff251ce284f1a5f1e39
Matías A. Bellone <matiasbellone@gmail.com>
2017-04-09 16:31:54 -0300

    Update CAD comic filter

--- plugins/af_comics/filters/af_comics_cad.php
+++ plugins/af_comics/filters/af_comics_cad.php
@@ -8,23 +8,19 @@ class Af_Comics_Cad extends Af_ComicFilter {
 	function process(&$article) {
 		$owner_uid = $article["owner_uid"];
 
-		if (strpos($article["link"], "cad-comic.com/cad/") !== FALSE) {
-			if (strpos($article["title"], "News:") === FALSE) {
+		if (strpos($article["link"], "cad-comic.com/comic/") !== FALSE) {
+			$doc = new DOMDocument();
+			@$doc->loadHTML(fetch_file_contents($article["link"]));
 
-				$doc = new DOMDocument();
-				@$doc->loadHTML(fetch_file_contents($article["link"]));
+			$basenode = false;
 
-				$basenode = false;
+			if ($doc) {
+				$xpath = new DOMXPath($doc);
+				$basenode = $xpath->query("(//a[@href='". $article["link"] ."']/img)")->item(0);
 
-				if ($doc) {
-					$xpath = new DOMXPath($doc);
-					$basenode = $xpath->query('(//img[contains(@src, "/comics/cad-")])')->item(0);
-
-					if ($basenode) {
-						$article["content"] = $doc->saveXML($basenode);
-					}
+				if ($basenode) {
+					$article["content"] = $doc->saveXML($basenode);
 				}
-
 			}
 
 			return true;
