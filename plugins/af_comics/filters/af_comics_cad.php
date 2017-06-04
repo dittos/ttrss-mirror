af2edfb08cd1e54b2c9869c7d7adbd796e03c0d9
aeritir <ttrss@headshot.de>
2017-06-04 20:52:19 +0200

    af_comics_cad: Update for Ctr+Alt+Del comic plugin

--- plugins/af_comics/filters/af_comics_cad.php
+++ plugins/af_comics/filters/af_comics_cad.php
@@ -6,18 +6,49 @@ class Af_Comics_Cad extends Af_ComicFilter {
 	}
 
 	function process(&$article) {
-		if (strpos($article["link"], "cad-comic.com/cad/") !== FALSE) {
-			if (strpos($article["title"], "News:") === FALSE) {
+		if (strpos($article["link"], "cad-comic.com/comic/") !== FALSE) {
+			if (strpos($article["title"], "News:") === FALSE)
+			if (in_array($article["tags"], "blog") === FALSE) {
 
 				$doc = new DOMDocument();
 
 				if (@$doc->loadHTML(fetch_file_contents($article["link"]))) {
 					$xpath = new DOMXPath($doc);
-					$basenode = $xpath->query('(//img[contains(@src, "/comics/cad-")])')->item(0);
+
+					$basenode = $xpath->query('(//div[@class="comicpage"])')->item(0);
+
+					// Remove unwanted previous/next comic links
+					foreach($xpath->query('//div[contains(attribute::class, "arrow")]') as $element ) {
+						$element->parentNode->removeChild($element);
+					}
+
+					// The CAD web site always uses HTTP for internal links and images
+					// so we fix it here if the article link points to HTTPS
+					if ($basenode && substr($article['link'], 0, 8 ) === "https://") {
+						foreach($basenode->getElementsByTagName('a') as $element) {
+							$element->setAttribute('href', preg_replace("/^http:/", "https:", $element->getAttribute('href')) );
+						}
+						foreach($basenode->getElementsByTagName('img') as $element) {
+							$element->setAttribute('src', preg_replace("/^http:/", "https:", $element->getAttribute('src')) );
+						}
+					}
+
+					// Get the text section (if present)
+					$post_text = $xpath->query('//div[@id="comicblog"]/div[@class="post-text-area"]')->item(0);
+					if ($post_text) {
+						if ($basenode) {
+							// Append to comic
+							$basenode->appendChild($post_text);
+						} else {
+							// Failsafe if no comic picture is present
+							$basenode = $post_text;
+						}
+					}
 
 					if ($basenode) {
 						$article["content"] = $doc->saveXML($basenode);
 					}
+
 				}
 
 			}
